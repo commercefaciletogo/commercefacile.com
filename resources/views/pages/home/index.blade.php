12ec27a8afd3b1634db18b9560fd134e6e9336fa
thomasdola <thomasdolar@gmail.com>
2017-04-22 10:53:53 +0000

    add jumbotron code

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -228,14 +228,14 @@
 
 @section('scripts')
     <script>
-//        $('.ads-slide').slick({
-//            // infinite: true,
-//            slidesToShow: 2,
-//            slidesToScroll: 1,
-//            variableWidth: true,
-//            autoplay: true,
+        $('.jumbotron-slide').slick({
+             infinite: true,
+             slidesToShow: 2,
+            slidesToScroll: 1,
+            variableWidth: true,
+            autoplay: true,
 //            autoplaySpeed: 5000
-//        });
+        });
     </script>
     <script src="https://b86068563707f1548c7c-cc73bb3144250bf95e4a0690bc25f5d2.ssl.cf5.rackcdn.com/assets/home.js"></script>
 @endsection
--- resources/views/partials/pages/home/_jumbo.blade.php
+++ resources/views/partials/pages/home/_jumbo.blade.php
@@ -6,8 +6,15 @@
         height: 350px;
         box-shadow: none;
         border-radius: 0;
+        padding: 0;
         font-size: 3em;
     ">
-
+        <div class="jumbotron-slide">
+            <img class="ui image" style="max-height: 350px;" src="{{asset('/img/jumbo_one.jpg')}}" alt="">
+            <img class="ui image" style="max-height: 350px;" src="{{asset('/img/jumbo_two.jpg')}}" alt="">
+            <img class="ui image" style="max-height: 350px;" src="{{asset('/img/jumbo_three.jpg')}}" alt="">
+            <img class="ui image" style="max-height: 350px;" src="{{asset('/img/jumbo_four.jpg')}}" alt="">
+            <img class="ui image" style="max-height: 350px;" src="{{asset('/img/jumbo_five.jpg')}}" alt="">
+        </div>
     </div>
 </div>
