8de305910317b4f2e84b5644636b72b417a5e8c0
thomasdola <thomasdolar@gmail.com>
2017-05-04 21:23:51 +0000

    change queue url by removing the port

--- config/queue.php
+++ config/queue.php
@@ -43,7 +43,7 @@ return [
 
         'beanstalkd' => [
             'driver' => 'beanstalkd',
-            'host' => env('QUEUE_HOST') . ':' . env('QUEUE_PORT'),
+            'host' => env('QUEUE_HOST'),
             'queue' => 'default',
             'retry_after' => 90,
         ],
