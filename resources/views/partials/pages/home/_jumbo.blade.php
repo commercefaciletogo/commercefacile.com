4b3c1a07c46af15c82c5bdebfa6a718376df7b8c
thomasdola <thomasdolar@gmail.com>
2017-06-11 11:08:21 +0000

    change jumbo images location to cloud

--- resources/views/partials/pages/home/_jumbo.blade.php
+++ resources/views/partials/pages/home/_jumbo.blade.php
@@ -10,9 +10,9 @@
         font-size: 3em;
     ">
         <div class="jumbotron-slide">
-            <img class="ui image" style="max-height: 350px;" src="{{asset('/img/jumbo_one.jpeg')}}" alt="">
-            <img class="ui image" style="max-height: 350px;" src="{{asset('/img/jumbo_two.jpeg')}}" alt="">
-            <img class="ui image" style="max-height: 350px;" src="{{asset('/img/jumbo_three.jpeg')}}" alt="">
+            <img class="ui image" style="max-height: 350px;" src="https://b86068563707f1548c7c-cc73bb3144250bf95e4a0690bc25f5d2.ssl.cf5.rackcdn.com/assets/img/jumbo_one.jpeg" alt="">
+            <img class="ui image" style="max-height: 350px;" src="https://b86068563707f1548c7c-cc73bb3144250bf95e4a0690bc25f5d2.ssl.cf5.rackcdn.com/assets/img/jumbo_two.jpeg" alt="">
+            <img class="ui image" style="max-height: 350px;" src="https://b86068563707f1548c7c-cc73bb3144250bf95e4a0690bc25f5d2.ssl.cf5.rackcdn.com/assets/img/jumbo_three.jpeg" alt="">
         </div>
     </div>
 </div>
